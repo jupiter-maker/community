19b17bb56fd96249533d550e716e739b2c358323
jupiter-maker <2572778568@qq.com>
2019-08-27 22:11:44 +0800

    last

--- src/main/java/com/jupiter/community/controller/AuthorizeController.java
+++ src/main/java/com/jupiter/community/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import com.jupiter.community.dto.GithubUser;
 import com.jupiter.community.model.User;
 import com.jupiter.community.provider.GithubProvider;
 import com.jupiter.community.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
+@Slf4j
 public class AuthorizeController {
 
     @Autowired
@@ -61,6 +63,7 @@ public class AuthorizeController {
             return "redirect:/";
         } else {
             //登录失败
+            log.error("callback get github error,{}",githubUser);
             return "redirect:/";
         }
     }
