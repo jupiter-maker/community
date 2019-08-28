e5d4a06c187f9b3553f0e8731f345f96e4030c4a
jupiter-maker <2572778568@qq.com>
2019-08-28 20:15:32 +0800

    last_3

--- src/main/java/com/jupiter/community/controller/FileController.java
+++ src/main/java/com/jupiter/community/controller/FileController.java
@@ -1,15 +1,17 @@
 package com.jupiter.community.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.jupiter.community.dto.FileDto;
 import com.jupiter.community.service.FtpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.Map;
 
 @Controller
@@ -20,27 +22,41 @@ public class FileController {
     private FtpService ftpService;
 
     @RequestMapping("/upload")
-    @ResponseBody
-    public FileDto upload(HttpServletRequest request){
-
+    public FileDto upload(HttpServletRequest request,
+                          HttpServletResponse response){
+        response.setContentType("text/html; charset=utf-8");
+        response.setCharacterEncoding("UTF-8");
         FileDto fileDto = new FileDto();
         MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest)request;
         MultipartFile file = multipartHttpServletRequest.getFile("editormd-image-file");
         if(file==null){
             fileDto.setSuccess(0);
-            fileDto.setMessage("图片上传失败");
-            return fileDto;
-        }
-        Map<String, String> result = ftpService.uploadPicture(file);
-        if("0".equals(result.get("error"))){
-            //上传成功
-            fileDto.setSuccess(1);
-            fileDto.setMessage("图片上传成功");
-            fileDto.setUrl(result.get("url"));
+            fileDto.setMessage("upload error");
+            try{
+                PrintWriter writer = response.getWriter();
+                writer.write(JSON.toJSONString(fileDto));
+                writer.close();
+            }catch(Exception e){
+            }
         }else{
-            fileDto.setSuccess(0);
-            fileDto.setMessage("图片上传失败");
+            Map<String, String> result = ftpService.uploadPicture(file);
+            if("0".equals(result.get("error"))){
+                //上传成功
+                fileDto.setSuccess(1);
+                fileDto.setMessage("upload success");
+                fileDto.setUrl(result.get("url"));
+            }else{
+                fileDto.setSuccess(0);
+                fileDto.setMessage("upload error");
+            }
+            try{
+                PrintWriter writer = response.getWriter();
+                writer.write(JSON.toJSONString(fileDto));
+                writer.close();
+            }catch(Exception e){
+            }
+
         }
-        return fileDto;
+        return null;
     }
 }
