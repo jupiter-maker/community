f4a0e1c421fa606c4f5a05917af321d856158fae
Jupiter <54050062+jupiter-maker@users.noreply.github.com>
2020-07-06 20:10:35 +0800

    Add files via upload

---
+++ community.sql
@@ -0,0 +1,90 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.49 : Database - community
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`community` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `community`;
+
+/*Table structure for table `community_comment` */
+
+DROP TABLE IF EXISTS `community_comment`;
+
+CREATE TABLE `community_comment` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint(20) NOT NULL COMMENT '父类id',
+  `type` int(11) DEFAULT NULL COMMENT '父类类型',
+  `commenttator` bigint(11) NOT NULL COMMENT '评论人id',
+  `gmt_create` bigint(20) NOT NULL COMMENT '创建时间',
+  `gmt_modefied` bigint(20) NOT NULL COMMENT '更新时间',
+  `like_count` int(20) DEFAULT '0',
+  `content` varchar(1024) DEFAULT NULL,
+  `comment_count` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `community_notification` */
+
+DROP TABLE IF EXISTS `community_notification`;
+
+CREATE TABLE `community_notification` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `notifier` bigint(20) NOT NULL COMMENT '通知的人',
+  `receiver` bigint(20) NOT NULL COMMENT '接收的人',
+  `outer_id` bigint(20) NOT NULL,
+  `type` int(11) NOT NULL COMMENT '类型',
+  `gmt_create` bigint(20) DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '状态',
+  `notifier_name` varchar(100) DEFAULT NULL,
+  `outer_title` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `community_question` */
+
+DROP TABLE IF EXISTS `community_question`;
+
+CREATE TABLE `community_question` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `description` text,
+  `gmt_create` bigint(20) DEFAULT NULL,
+  `gmt_modified` bigint(20) DEFAULT NULL,
+  `creator` bigint(11) DEFAULT NULL,
+  `comment_count` int(11) DEFAULT '0',
+  `view_count` int(11) DEFAULT '0',
+  `like_count` int(11) DEFAULT '0',
+  `tag` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `community_user` */
+
+DROP TABLE IF EXISTS `community_user`;
+
+CREATE TABLE `community_user` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `account_id` varchar(100) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `token` char(36) DEFAULT NULL,
+  `gmt_create` bigint(20) DEFAULT NULL,
+  `gmt_modified` bigint(20) DEFAULT NULL,
+  `bio` varchar(256) DEFAULT NULL,
+  `avatar_url` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
